a3384e11753b0f0fc821bd8e607fbbe78d374873
Barnabas Asha <79143327+Barnabas-Starks@users.noreply.github.com>
2021-05-29 18:43:58 +0100

    Responsive issues, error handling, svg minifys, etc

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
-import 'regenerator-runtime/runtime'
-const token = process.env.GITHUB_APP_API_KEY
+import "regenerator-runtime/runtime";
+const token = process.env.GITHUB_APP_API_KEY;
 
 async function fetchData() {
-  const userName = await sessionStorage.getItem('userName')
+  const userName = await sessionStorage.getItem("userName");
   const reqBody = {
     query: `
         query { 
@@ -32,27 +32,30 @@ async function fetchData() {
           }
         }
             `,
-  }
- if(userName) {
-  try {
-    fetch("https://api.github.com/graphql", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(reqBody),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-          renderData(data.data)
-      }, (e) => console.log(e.message))
-  } catch(e) {
-    console.log(e.message)
-  }
- }
-}
+  };
 
+  if (userName) {
+    try {
+      fetch("https://api.github.com/graphql", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(reqBody),
+      })
+        .then((res) => res.json())
+        .then(
+          (data) => {
+            renderData(data.data);
+          },
+          (e) => console.log(e.message)
+        );
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+}
 
 function renderData(data) {
   const header_profile_img = document.querySelector(".header_profile_img");
@@ -60,25 +63,31 @@ function renderData(data) {
   const user_bio_img = document.querySelector(".user-bio_img");
   const sticky_avatar = document.querySelector(".sticky_avatar");
   const sticky_username = document.querySelector(".sticky_username");
-  const repositories = document.querySelector(".repositories_cluster");
+  const repositories_cluster = document.querySelector(".repositories_cluster");
   const count = document.querySelector(".count");
   const counter = document.querySelector(".counter");
 
-  user_bio.querySelector(".fname").textContent = data.user.name;
-  user_bio.querySelector(".username").textContent = data.user.login;
-  user_bio.querySelector(".user_description").textContent = data.user.bio;
-  user_bio_img.querySelector("img").setAttribute("src", data.user.avatarUrl);
-  sticky_avatar.querySelector("img").setAttribute("src", data.user.avatarUrl);
-  sticky_username.textContent = data.user.login;
+  const { name, login, bio, avatarUrl, repositories } = data.user
+  user_bio.querySelector(".fname").textContent = name;
+  user_bio.querySelector(".username").textContent = login;
+  user_bio.querySelector(".user_description").textContent = bio;
+  user_bio_img.querySelector("img").setAttribute("src", avatarUrl);
+  user_bio_img.querySelector("img").setAttribute("alt", name);
+  sticky_avatar.querySelector("img").setAttribute("src", avatarUrl);
+  sticky_avatar.querySelector("img").setAttribute("alt", name);
+
+  sticky_username.textContent = login;
   header_profile_img
     .querySelector("img")
-    .setAttribute("src", data.user.avatarUrl);
-  counter.textContent = data.user.repositories.nodes.length;
-  count.textContent = data.user.repositories.nodes.length;
+    .setAttribute("src", avatarUrl);
+    header_profile_img
+    .querySelector("img")
+    .setAttribute("alt", name);
+  counter.textContent = repositories.nodes.length;
+  count.textContent = repositories.nodes.length;
 
-  data.user.repositories.nodes.map((repo) => {
+  repositories.nodes.map((repo) => {
     const {
-      name,
       description,
       primaryLanguage,
       stargazerCount,
@@ -90,7 +99,7 @@ function renderData(data) {
     repo_entity.className = "repo_entity";
     repo_entity.innerHTML = `
                             <div class="col1">
-                              <a href="#" class="repo-title">${name}</a>
+                              <a href="#" class="repo-title">${repo.name}</a>
                               <p class="repo-desc">${
                                 description === null ? "" : description
                               }</p>
@@ -110,18 +119,13 @@ function renderData(data) {
                                  
                                   <span class="meta_language effect">
                                   <span class="meta_language-stars">
-                                  <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd"
-                                      d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z" />
-                                  </svg>
+                                  <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"/></svg>
                                   </span>
                                   <span>${stargazerCount}</span>
                                   </span>
                                   <span class="meta_language effect">
                                   <span class="meta_language-forks">
-                                  <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
-                                      <path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"/>
-                                  </svg>
+                                  <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"/></svg>
                                   </span>
                                   <span>${forkCount}</span>
                                   </span>
@@ -134,12 +138,12 @@ function renderData(data) {
                               </div>
                           </div>
                           <div class="col2">
-                              <button class="repo-action">
-                                  <i class="far fa-star"></i> <span class="action-text">Star</span>
+                              <button class="repo-action flex">
+                                  <svg class="repo-action_icon" width="16" height="16" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"/></svg> <span class="action-text">Star</span>
                               </button>
                           </div>
          `;
-    repositories.append(repo_entity);
+    repositories_cluster.append(repo_entity);
   });
 }
 
@@ -194,7 +198,7 @@ const handleTime = (updatedAt) => {
 function handleSticky() {
   const observerOptions = {
     root: null,
-    threshold: 0.5,
+    threshold: 0.4,
   };
 
   const observer = new IntersectionObserver(displaySticky, observerOptions);
@@ -213,14 +217,11 @@ function handleSticky() {
 }
 
 window.onload = async () => {
-    const userName = await sessionStorage.getItem("userName");
-    if (!userName) {
-      window.location.href = "/";
-    } else {
-      fetchData()
-      handleSticky()
-    }
-  };
-
-  
-  
+  const userName = await sessionStorage.getItem("userName");
+  if (!userName) {
+    window.location.href = "/";
+  } else {
+    fetchData();
+    handleSticky();
+  }
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,71 @@
-import 'regenerator-runtime/runtime'
-// const token = process.env.GITHUB_APP_API_KEY;
+import "regenerator-runtime/runtime";
+const token = process.env.GITHUB_APP_API_KEY;
 
-const myForm = document.getElementById('view-profile');
-let errors = []
+const myForm = document.getElementById("view-profile");
+let errors = [];
 
 myForm.onsubmit = function (e) {
-    e.preventDefault()
-    const { userName } = this.elements;
-    sessionStorage.setItem('userName', userName.value)
-    window.location.href = '/src/pages/profile.html'
+  e.preventDefault();
+  const { userName } = this.elements;
+  checkUser(userName)
+};
+
+function checkUser(user) {
+  const reqBody = {
+    query: `
+            query { 
+              user (login: "${user.value}") { 
+                login
+              }
+            }
+            `,
+  };
+
+  if (user) {
+    try {
+      fetch("https://api.github.com/graphql", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(reqBody),
+      })
+        .then((res) => res.json())
+        .then(
+          (data) => {
+              if(data.errors) {
+                  errors.push(data.errors[0].message)
+                  showError()
+              }else {
+                  user.value = ""
+                const userName = data.data.user.login
+                sessionStorage.setItem("userName", userName);
+                window.location.href = "/src/pages/profile.html";
+              }
+          },
+          (e) => {
+              errors.push(e.message)
+              showError()
+          }
+        );
+    } catch (e) {
+        errors.push(e.message)
+        showError()
+    }
+  }
 }
 
+const errorElement = document.querySelector('.errors')
 
-// const errorElement = document.querySelector('.errors')
-
-// function showError () {
-//   if(errors.length) {
-//     errorElement.innerHTML = `
-//       ${ errors.map( error => `<li>${error}</li>`)}
-//     `
-//   }
-//   setTimeout(() => {
-//     errors = []
-//     errorElement.innerHTML = null
-//   }, 1500)
-// }
-
+function showError () {
+  if(errors.length) {
+    errorElement.innerHTML = `
+      ${ errors.map( error => `<li>${error}</li>`)}
+    `
+  }
+  setTimeout(() => {
+    errors = []
+    errorElement.innerHTML = null
+  }, 2500)
+}
